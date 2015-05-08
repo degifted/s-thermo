a173cd49bcf173971c945e667735496be0b3ffef
Dmitriy Gorokh <degifted@gmail.com>
2015-05-08 03:54:49 +0300

    auto commit by make

--- main.c
+++ main.c
@@ -179,8 +179,7 @@ ISR (TIMER2_COMP_vect)
 // Realtime clock
 ISR (TIMER1_COMPA_vect)
 {
-    cnt3++;
-    if (cnt3 > 1)
+    if (cnt3++ > 2)
         heaterOff();
     if (cnt1++ == 240)
         cnt1 = 0;
