86c8089846a48206a54cf6739bb2ecba6dcd4d21
Dmitriy Gorokh <degifted@gmail.com>
2015-05-15 02:37:04 +0300

    auto commit by make

--- config.h
+++ config.h
@@ -39,9 +39,9 @@
 #define     PREHEAT_ENERGY                      1000     // Amount of energy for preheat
                                                          // per each degree of an error.
                                                          // 700 means 7 seconds of full power per each degree.
-#define     PREHEAT_START_THRESHOLD             7        // Minimum difference between target and current
+#define     PREHEAT_START_THRESHOLD             9        // Minimum difference between target and current
                                                          // temperature at which the preheat starts.
-#define     PREHEAT_STOP_THRESHOLD              3        // A safety threshold of the error at which
+#define     PREHEAT_STOP_THRESHOLD              5        // A safety threshold of the error at which
                                                          // the preheat switches off (abnormal situation).
 #define     PID_A                               100      // PID output attenuation (a reciprocal to gain).
 #define     PID_P                               13       // PID proportional coefficient.
@@ -53,14 +53,14 @@
 #define     PID_I_N_LIMIT                      -5        // derivative components. Beyond these limits
 #define     PID_D_P_LIMIT                       20       // the corresponding value is saturated.
 #define     PID_D_N_LIMIT                      -60        
-#define     PID_INTEGRATOR_BAND                 2        // PID error boundaries inside which the
+#define     PID_INTEGRATOR_BAND                 1        // PID error boundaries inside which the
                                                          // integrator starts working.
 #define     PID_UPPER_REGULATION_LIMIT          1        // PID maximum negative error, beyond which
                                                          // the PID regulator is switched off (abnormal situation).
 #define     MAXIMUM_TEMPERATURE_CHANGE_RATE     20       // Maximum allowed speed of temperature change per
                                                          // PID_D_INTERVAL. If the temperature is changing too
                                                          // fast, there is something wrong with the setup.
-#define     MAXIMUM_TEMPERATURE                 95       // Temperature limits for the set point.
+#define     MAXIMUM_TEMPERATURE                 85       // Temperature limits for the set point.
 #define     MINIMUM_TEMPERATURE                 25
 #define     MAXIMUM_ALLOWED_OVERHEAT            5        // If the temperature goes beyond that limit,
                                                          // the regulation process must be aborted.
